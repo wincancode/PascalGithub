9606eb54b11c1995615821d573579ecf7d02430d
Your Name <erodriguez.win@gmail.com>
2022-05-21 21:56:44 -0400

    Creacion del segundo Problema del Postlaboratorio

---
+++ DescendienteYAscendente.pas
@@ -0,0 +1,23 @@
+
+Program DescendienteYAscendente;
+
+Uses Crt;
+ 
+
+Begin
+
+End.
+
+
+{2.- Construya un programa que lea los elementos de una secuencia de números enteros que termina con un 0. Si la secuencia es estrictamente descendente el programa debe escribir el mensaje “Su lista de N elementos es descendente”, donde N es la cantidad de elementos de la lista, incluyendo el 0. Si la secuencia no es estrictamente descendente debe escribir el mensaje “Su lista de N elementos no es descendente, ya que aparece A seguido de B”,donde A y B son dos números que aparecen en la secuencia, A inmediatamente antes de B y B no es menor que A.
+
+Por ejemplo, si la secuencia en la entrada es 10, 9, 4, 2, 0, la salida debe ser :
+Su lista de 5 elementos es descendente.
+
+Por otra parte, si la secuencia en la entrada es 8, 6, 5, 7, 6, 8, 0, la salida debe ser:
+Su lista de 7 elementos NO es descendente, ya que aparece 5 seguido de 7.
+
+De igual manera para la lista 6, 3, 3, 0, la salida debe ser:
+Su lista de 4 elementos NO es descendente ya que aparece 3 seguido de 3.
+
+Nota : Para la corrida de este programa usar los datos de los ejemplo dados.}
