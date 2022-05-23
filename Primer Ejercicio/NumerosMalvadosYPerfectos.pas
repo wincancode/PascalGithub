6d6e3fe21dc7c7e1f56790dd190a24969852fb75
Your Name <erodriguez.win@gmail.com>
2022-05-23 10:41:33 -0400

    var documentation and name change to short versions.

--- Primer Ejercicio/NumerosMalvadosYPerfectos.pas
+++ Primer Ejercicio/NumerosMalvadosYPerfectos.pas
@@ -1,68 +1,67 @@
 
 
-Program NumerosPerfectos_Y_NumerosMalvados;
+Program NumerosPerf_Y_NumerosMalvados;
 
 Uses Crt;
 
 Var 
-  Numero,AuxBin,AuxilairPerfecto,AuxBin2,Perfectos,malvados,
-  MayorPerfecto,MayorMalvado,MenorPerfecto,MenorMalvado,i:  Integer;
+  //AuxBin,AuxBin2: Auxiliares para emular la obtencion del numero binario para verificar los numeros malvados; AuxP: Auxiliar para verificar los numeros perfectos; Perf,Malv: Contador de perfectos y malvados; MayorP,MayorM,MenorP,MenorM: Mayores y menores de perfectos y malvados; i: Contador general.
+  Num,AuxBin,AuxBin2,AuxP,Perf,Malv,
+  MayorP,MayorM,MenorP,MenorM,i:  Integer;
 
-  PromedioPerfectos,PromedioMalvados:  Real;
+  //PP: Promedio de Perfectos; PM: Promedio de malvados
+  PP,PM:  Real;
 Begin
 
-  //Tqm Edui
-  j := 0;
-  perfectos := 0;
-  malvados := 0;
-  AuxilairPerfecto := 0;
-  PromedioMalvados := 0;
-  PromedioPerfectos := 0;
+
+  Perf := 0;
+  Malv := 0;
+  AuxP := 0;
+  PM := 0;
+  PP := 0;
   AuxBin2 := 0;
 
-  MayorMalvado  := 0;
-  MayorPerfecto := 0;
+  MayorM  := 0;
+  MayorP := 0;
 
-  Writeln('Este programa calcula el promedio, el mayor, y el menor de los numeros perfectos y malvados de una secuencia numerica. ');
+  Writeln('Este programa calcula el promedio, el mayor, y el menor de los numeros perfectos y Malvados de una secuencia numerica. ');
   Writeln('Ingrese una secuencia de numeros, utilizando el cero (0) para terminarla.');
-  Readln(Numero);
+  Readln(Num);
 
 
 
-  While Numero<>0 Do
+  While Num<>0 Do
     Begin
 
-      If Perfectos=0 Then
-        MenorPerfecto := Numero+1;
-      If Malvados=0 Then
-        MenorMalvado := Numero+1;
+      If Perf=0 Then
+        MenorP := Num+1;
+      If Malv=0 Then
+        MenorM := Num+1;
 
-
-
-      //*Se verifican los numeros perfectos
-      AuxilairPerfecto := 0;
-      For i := 1 To Numero-1 Do
+      //*Se verifican los numeros Perfectos
+      AuxP := 0;
+      For i := 1 To Num-1 Do
         Begin
-          If Numero Mod i = 0 Then
+          If Num Mod i = 0 Then
             Begin
-              AuxilairPerfecto := AuxilairPerfecto+i;
+              AuxP := AuxP + i;
             End;
         End;
 
-      //*Se cuentan y acumulan los perfectos, y se verifica si hay un nuevo mayor y menor;
-      If AuxilairPerfecto = Numero Then
+      //*Se cuentan y acumulan los Perfectos, y se verifica si hay un nuevo mayor y menor;
+      If AuxP = Num Then
         Begin
-          Perfectos := Perfectos+1;
-          PromedioPerfectos := PromedioPerfectos+Numero;
-          If Numero > MayorPerfecto Then
-            MayorPerfecto := Numero;
-          If Numero < MenorPerfecto Then
-            MenorPerfecto := Numero;
+          Perf := Perf+1;
+          PP := PP+Num;
+          If Num > MayorP Then
+            MayorP := Num;
+          If Num < MenorP Then
+            MenorP := Num;
         End;
 
 
-      //*Se verifican los numeros malvados
-      AuxBin := Numero;
+      //*Se verifican los numeros Malvados
+      AuxBin := Num;
       i := 0;
 
       Repeat
@@ -75,41 +74,41 @@ Begin
       Until (AuxBin=0);
 
 
-      //*Se cuentan y acumulan los malvados, y se verifica si hay un nuevo mayor y menor;
+      //*Se cuentan y acumulan los Malvados, y se verifica si hay un nuevo mayor y menor;
 
       If i Mod 2 = 0 Then
         Begin
-          Malvados := Malvados+1;
-          PromedioMalvados := PromedioMalvados+Numero;
-          If Numero > MayorMalvado Then
-            MayorMalvado := Numero;
-          If Numero < MenorMalvado Then
-            MenorMalvado := Numero;
+          Malv := Malv + 1;
+          PM := PM+Num;
+          If Num > MayorM Then
+            MayorM := Num;
+          If Num < MenorM Then
+            MenorM := Num;
 
         End;
 
       Writeln('Ingrese otro numero.');
-      Readln(Numero);
+      Readln(Num);
     End;
 
-  If Perfectos<>0 Then
+  If Perf<>0 Then
     Begin
-      PromedioPerfectos := PromedioPerfectos/Perfectos;
+      PP := PP/Perf;
       Writeln('Numeros Perfectos: ');
-      Writeln('El mayor es: ', MayorPerfecto);
-      Writeln('El menor es: ', MenorPerfecto);
-      Writeln('Y su promedio fue de: ',PromedioPerfectos:0:2);
+      Writeln('El mayor es: ', MayorP);
+      Writeln('El menor es: ', MenorP);
+      Writeln('Y su promedio fue de: ',PP:0:2);
     End
   Else
     Writeln('No hubieron Numeros Perfectos');
   Writeln('---------------------------------');
-  If Malvados<>0 Then
+  If Malv<>0 Then
     Begin
-      PromedioMalvados := PromedioMalvados/malvados;
+      PM := PM/Malv;
       Writeln('Numeros Malvados: ');
-      Writeln('El mayor es: ', MayorMalvado);
-      Writeln('El menor es: ',MenorMalvado);
-      Writeln('Y su promedio fue de: ',PromedioMalvados:0:2);
+      Writeln('El mayor es: ', MayorM);
+      Writeln('El menor es: ',MenorM);
+      Writeln('Y su promedio fue de: ',PM:0:2);
     End
   Else
     Writeln('No hubieron Numeros Malvados');
@@ -123,16 +122,22 @@ End.
 
 
 
+
+
+
+
+
+
 {!1.- Dado una secuencia de números naturales, finalizada con un cero, construya un
-algoritmo que determine : el mayor, el menor y el promedio de los números perfectos y
-a su vez malvados, según la definición dada a continuación.
+algoritmo que determine : el mayor, el menor y el promedio de los números Perf y
+a su vez Malv, según la definición dada a continuación.
 
 Número perfecto: es todo número natural que es igual a la suma de sus divisores propios
 (es decir, todos sus divisorees excepto el propio número). Por ejemplo, 6 es un número
 perfecto ya que sus divisores propios son 1, 2, y 3 y se cumple que 1+2+3=6.
 
 Número malvado: todo número natural cuya expresión en base 2 (binaria) contiene un
-número par de unos. Por ejemplo el 12 y el 15 son números malvados ya que 12=1100
+número par de unos. Por ejemplo el 12 y el 15 son números Malv ya que 12=1100
 contiene dos(2) unos en su representación binaria y 15=1111 contiene cuatro(4) unos
 en su representación binaria ( dos y cuatro son pares).
 
