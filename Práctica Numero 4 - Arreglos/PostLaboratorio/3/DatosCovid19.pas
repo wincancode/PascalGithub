cb7f98da03ff7249bd154a7a2271c66c373112fe
Your Name <erodriguez.win@gmail.com>
2022-06-15 19:52:04 -0400

    subida del tercer ejercicio

--- Práctica Numero 4 - Arreglos/PostLaboratorio/3/DatosCovid19.pas
+++ Práctica Numero 4 - Arreglos/PostLaboratorio/3/DatosCovid19.pas
@@ -4,15 +4,26 @@ Program DatosCovid19;
 Uses Crt;
 
 Const 
+  //Se crean constantes con los generos, estados del pais y meses del anio para mejor escritura en pantalla. 
   Generos: array[1..2] Of string[7] = ('Hombres','mujeres');
-  Estados: array[1..23] Of string[16] = ('Amazonas','Anzoategui','Apure','Aragua','Barinas','Bolivar','Carabobo','Cojedes','Delta Amacuro',{'Distrito Capital',}'Falcon','Guarico','Lara','Merida','Miranda','Monagas','Nueva Esparta','Portuguesa','Sucre','Tachira','Trujillo','Vargas','Yaracuy','Zulia');
+  //Se denota que 1: Hombres y 2: Mujeres de esta forma.
+  Estados: array[1..23] Of string[16] = ('Amazonas','Anzoategui','Apure','Aragua','Barinas','Bolivar','Carabobo','Cojedes','Delta Amacuro','Falcon','Guarico','Lara','Merida','Miranda','Monagas','Nueva Esparta','Portuguesa','Sucre','Tachira','Trujillo','Vargas','Yaracuy','Zulia');
   meses: array[1..12] Of string[10] = ('enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiempre','octubre','noviembre','diciembre');
 
 Var 
+
+  {CASOS: Arreglo tridimensional donde se encuentran la cantidad de casos ordenados por estado del pais, mes del anio y genero.
+   ContagiosM: Los contagios de mujeres por estado
+   ContagiosH: Los contagios de hombres en un estado dado
+   EstadoMayor: El estado con mayor numero de contagios
+   CasosEstadoMay: Los casos por estado del estado mayor para lograr comparar entre ellos. }
   CASOS: Array[1..23,1..12,1..2] Of integer;
-  ContagioM,ContagioH,EstadoMayor,i,j,k : Integer;
+  ContagiosM: Array [1..23] Of integer;
+  ContagioH,EstadoMayor,MesHombres,CasosEstado,CasosEstadoMay,i,j,k : Integer;
 Begin
 
+  Clrscr;
+  //Se inicializan los arreglos y variables
   For i:=1 To 23 Do
     Begin
       For j := 1 To 12 Do
@@ -24,12 +35,19 @@ Begin
         End;
     End;
 
+  For i:=1 To 23 Do
+    Begin
+      ContagiosM[i] := 0;
+    End;
 
-  For i:=1 To 5 Do
-    //1-23
+  ContagioH := 0;
+  CasosEstadoMay := -1;
+  CasosEstado := 0;
+
+  //se piden los datos al usuario.
+  For i:=1 To 23 Do
     Begin
       For j := 1 To 12 Do
-        //1-12
         Begin
           For k := 1 To 2 Do
             Begin
@@ -39,20 +57,64 @@ Begin
         End;
     End;
 
+  clrscr;
+  Writeln('De que mes desea obtener la cantidad de hombres contagiados?');
+  For j:=1 To 12 Do
+    Begin
+      Writeln(j,'.',Meses[j]);
+    End;
+  Readln(MesHombres);
 
 
+  //Se obtienen la cantidad de mujeres contagiadas en cada estado.
+  For i:=1 To 23 Do
+    Begin
+      For j := 1 To 12 Do
+        Begin
+          ContagiosM[i] := ContagiosM[i]+CASOS[i,j,2];
+        End;
+    End;
 
+  //Se obtienen la cantidad de hombres contagiados en un mes dado
+  For i:=1 To 23 Do
+    Begin
+      ContagioH := ContagioH + casos[i,MesHombres,1];
+    End;
 
+  //Se obtiene cual es el estado con mayor numero de contagios.
+  For i:= 1 To 23 Do
+    Begin
+      CasosEstado := 0;
+      For j:=1 To 12 Do
+        Begin
+          For k:=1 To 2 Do
+            Begin
+              CasosEstado := CasosEstado + CASOS[i,j,k];
+            End;
+        End;
+      If CasosEstado > CasosEstadoMay Then
+        Begin
+          EstadoMayor := i;
+          CasosEstadoMay := CasosEstado;
+        End;
 
+    End;
 
+  clrscr;
+
+  //Se muestran los datos en pantalla.
+  Writeln('Esta es la cantidad de mujeres contagiadas en cada estado.');
+  For i:=1 To 23 Do
+    Begin
+      Writeln(estados[i],': ',ContagiosM[i]);
+    End;
+
+  Writeln(' ');
+
+  Writeln('Cantidad de hombres contagiados en el mes de ',Meses[MesHombres],': ',ContagioH);
+
+  Writeln('El estado con mayor numero de contagios es: ',Estados[EstadoMayor]);
+
+  Readkey();
 
 End.
-
-
-Un arreglo tridimensional contiene la cantidad de hombres y mujeres que se
-han reportado con COVID-19 , durante los 12 meses del año, en cada uno de
-los 23 estados del país. Donde CASOS [I, J, K] representa la cantidad de casos
-que se reportaron en el estado I, el mes J, de sexo K. Encontrar
-a) Cantidad de mujeres contagiados en cada estado.
-c) Cantidad de hombres contagiadas durante un mes dado.
-b) El estado con mayor número de contagio.  5 ptos
